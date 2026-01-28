b761b0838ba607388e9d7652d6dec8a74de83e10
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:25:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # ==============================
 API_ID = int(os.getenv("API_ID", "22657083"))
 API_HASH = os.getenv("API_HASH", "d6186691704bd901bdab275ceaab88f3")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7883129570:AAENeLvTsdrAmFxFgzW0yYSP2RMsQ4TElp4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8041174184:AAFsZEgcmkuM2enBfGyv4lLipuaxYMtdYKo")
 
 # ==============================
 # Bot Details
