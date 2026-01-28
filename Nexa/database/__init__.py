f17f4cd8c50b23effb32ac4323f5a2ceb514f69d
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 22:32:03 +0530

    Update __init__.py

--- Nexa/database/__init__.py
+++ Nexa/database/__init__.py
@@ -2,3 +2,13 @@ from . import users
 from . import sessions
 from . import otp_codes
 from . import deposits   
+
+
+from .sessions import (
+    add_session,
+    remove_session,
+    revoke_session,
+    get_session,
+    get_available_session,  # <-- add here
+    ...
+)
