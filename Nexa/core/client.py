4aafd0911ab65f9155661473e25ef9d56d67ec53
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 13:37:50 +0530

    Update client.py

--- Nexa/core/client.py
+++ Nexa/core/client.py
@@ -1,13 +1,10 @@
-from pyrogram import Client, enums
+from pyrogram import Client
 import config
 
 app = Client(
-    name="NexaBot",
+    "NexaBot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
-    workers=10,
-    in_memory=True,
-    parse_mode=enums.ParseMode.MARKDOWN,
     plugins=dict(root="Nexa.plugins")
 )
