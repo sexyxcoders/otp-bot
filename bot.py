3c709c3574591ac37be57a4410fc69b386656979
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:56:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
+# bot.py
 from Nexa.core.client import app
 
-# Import user plugins
-import Nexa.plugins
-
-# Import admin plugins
-import Nexa.Admin
+# Import all plugins manually
+import Nexa.plugins        # regular user plugins
+import Nexa.Admin          # admin plugins
 
 if __name__ == "__main__":
     print("🚀 Bot is starting...")
