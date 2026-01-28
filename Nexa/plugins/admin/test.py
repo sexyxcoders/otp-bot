f05ee8a6e8593fa1b4c9a658e415b360b1cd6a74
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:50:32 +0530

    Delete Nexa/plugins/admin directory

--- Nexa/plugins/admin/test.py
+++
@@ -1,6 +0,0 @@
-from Nexa.core.client import app
-from pyrogram import filters
-
-@app.on_message(filters.private)
-async def test_cmd(_, msg):
-    await msg.reply_text("✅ BOT IS RECEIVING MESSAGES")
