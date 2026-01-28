c3e40aba75a24e23c31c9817735d7f5bef3d3460
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 22:20:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
-# bot.py
 from Nexa.core.client import app
-
-# Import all plugins so Pyrogram registers handlers
-import Nexa.plugins           # user-facing plugins
-import Nexa.plugins.admin     # admin plugins
+import Nexa.plugins  # load all plugins
 
 if __name__ == "__main__":
-    print("🚀 Bot is starting...")
+    print("🚀 OTP Bot is starting...")
     app.run()
