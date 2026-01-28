8cb1ee9aca3a01b98ec0d11b8298283645d7d576
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:37:45 +0530

    Delete test_root.py

--- Nexa/plugins/test_root.py
+++
@@ -1,9 +0,0 @@
-from Nexa.core.client import app
-from pyrogram import filters
-
-print(">>> TEST ROOT PLUGIN LOADED <<<")
-
-@app.on_message()
-async def debug_receive(client, msg):
-    print(">>> MESSAGE RECEIVED:", msg.text, msg.from_user.id)
-    await msg.reply_text(f"✅ RECEIVED: {msg.text}")
