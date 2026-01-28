fed436138321252af2cf07dffcb2339255a01109
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 14:07:15 +0530

    Update __init__.py

--- Nexa/plugins/__init__.py
+++ Nexa/plugins/__init__.py
@@ -1,24 +1,16 @@
-# User modules
+# Plugins __init__.py
+# Ensures all plugin modules are loaded
+
+# User-facing modules
 from .start import *
 from .profile import *
 from .deposit import *
 from .referral import *
-from .order_history import *
+from .history import *
 from .wallet import *
 
-# ================= ADMIN MODULES =================
+# Sessions
+from .sessions import *
 
-from .admin.panel import *
-
-from .admin.country import *
-from .admin.price import *
-
-from .admin.sessions import *
-from .admin.stock import *
-from .admin.revoke_session import *
-
-from .admin.deposits import *
-
-
-from .admin.broadcast import *
-from .admin.referral import *
+# Admin modules
+from .admin import *
