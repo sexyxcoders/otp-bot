47e9ee10a167e6b3c0bee51770f5aad74b321e65
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 22:25:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,17 @@ import os
 # ----------------------------
 # Bot / API
 # ----------------------------
-API_ID = int(os.getenv("API_ID", "123456"))        # Telegram API ID
-API_HASH = os.getenv("API_HASH", "your_api_hash") # Telegram API Hash
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "22657083"))        # Telegram API ID
+API_HASH = os.getenv("API_HASH", "d6186691704bd901bdab275ceaab88f3") # Telegram API Hash
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7883129570:AAENeLvTsdrAmFxFgzW0yYSP2RMsQ4TElp4")
 
 # ----------------------------
 # MongoDB
 # ----------------------------
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://krishvip39_db_user:Nqg0y3VXnfvacKRv@cluster0.e4cxyeg.mongodb.net/?appName=Cluster0")
 
 # ----------------------------
 # Admins
 # ----------------------------
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split()]  # space-separated IDs
+ADMINS = [int(x) for x in os.getenv("ADMINS", "8553815122").split()]  # space-separated IDs
 
-# ----------------------------
-# Referral
-# ----------------------------
-REFERRAL_PERCENT = int(os.getenv("REFERRAL_PERCENT", "5"))
