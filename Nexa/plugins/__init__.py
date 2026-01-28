13ff1bee13c083ee35d2cbd0179a1dd706d58dd1
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:00:32 +0530

    Update __init__.py

--- Nexa/plugins/__init__.py
+++ Nexa/plugins/__init__.py
@@ -1,31 +1,4 @@
-# Nexa/plugins/__init__.py
-"""
-Plugin loader for Nexa Bot
-Imports all user and admin plugins safely
-"""
+# Root plugins package
+# Pyrogram will auto-load all plugin files from this directory
 
-# -------------------
-# User-facing plugins
-# -------------------
-from . import start
-from . import profile
-from . import deposit
-from . import referral
-from . import wallet
-from . import order_history
-from . import sessions  # user sessions
-
-# -------------------
-# Admin plugins
-# -------------------
-from .admin import panel
-from .admin import users
-from .admin import deposits
-from .admin import history
-from .admin import referral
-from .admin import broadcast
-from .admin import sessions as admin_sessions
-from .admin import stock
-from .admin import country
-from .admin import price
-from .admin import revoke_session
+print(">>> ROOT PLUGINS LOADED <<<")
