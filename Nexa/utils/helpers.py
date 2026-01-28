be68cbd04afd8188b9b7057da564d93f46dee417
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 15:39:37 +0530

    Delete Nexa directory

--- Nexa/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-from Nexa.core.client import app
-import config
-
-async def log_admin_action(text):
-    try:
-        await app.send_message(config.LOGS_CHANNEL, text)
-    except Exception as e:
-        print("Log error:", e)
