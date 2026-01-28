628c9695c4fc572771627ea4fd88bede0af7d251
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 21:59:05 +0530

    Update mongo.py

--- Nexa/database/mongo.py
+++ Nexa/database/mongo.py
@@ -1,16 +1,5 @@
 from pymongo import MongoClient
 import config
 
-MONGO_URL = config.MONGO_URL  # add this in config.py
-
-client = MongoClient(MONGO_URL)
-db = client["nexa_db"]
-
-users = db["users"]
-orders = db["orders"]
-deposits = db["deposits"]
-referrals = db["referrals"]  # ADD THIS
-sessions = db["sessions"]
-countries = db["countries"]
-otp_codes = db["otp_codes"]
-counters = db["counters"]
+client = MongoClient(config.MONGO_URL)
+db = client["nexa_bot"]
