c5f4386146505dd245cacb6754cc46a458113199
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 15:40:28 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-# bot.py
-from Nexa.core.client import app
-
-# Import plugins after app is ready
-import Nexa.plugins
-
-if __name__ == "__main__":
-    print("🚀 Bot is starting...")
-    app.run()
