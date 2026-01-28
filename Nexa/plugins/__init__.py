2b34f42d94bb28f9e32a1e1259f2ccebc3496fbb
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 17:24:12 +0530

    Update __init__.py

--- Nexa/plugins/__init__.py
+++ Nexa/plugins/__init__.py
@@ -1,7 +1,7 @@
 # Root plugins package
-# Pyrogram will auto-load all plugin files from this folder
+# Only import root modules here
+# Do NOT import admin plugins
 
-# Import root plugins explicitly (optional, for clarity)
 from . import deposit
 from . import referral
 from . import order_history
@@ -9,8 +9,5 @@ from . import sessions
 from . import wallet
 from . import profile
 from . import start
-  # if you have a test plugin
-
-# Do NOT import admin plugins here; keep root & admin separate
 
 print(">>> ROOT PLUGINS LOADED <<<")
