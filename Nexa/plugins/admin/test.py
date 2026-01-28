ab65b3beacc48a9a73f24ca48a275e5e2f481884
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-28 16:42:00 +0530

    Delete test.py

--- Nexa/plugins/admin/test.py
+++
@@ -1,6 +0,0 @@
-from Nexa.core.client import app
-
-@app.on_message()
-async def test_receive(client, msg):
-    print("Received from:", msg.from_user.id)
-    await msg.reply_text(f"Got your message: {msg.text or 'media'}")
