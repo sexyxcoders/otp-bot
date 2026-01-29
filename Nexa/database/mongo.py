665d56a01e539e031d6b2e6e4c46ed03f5547d7c
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-01-29 13:12:25 +0530

    Update mongo.py

--- Nexa/database/mongo.py
+++ Nexa/database/mongo.py
@@ -1,5 +1,15 @@
-from pymongo import MongoClient
-import config
+import motor.motor_asyncio
+import os
+from dotenv import load_dotenv
 
-client = MongoClient(config.MONGO_URL)
-db = client["nexa_bot"]
+load_dotenv()
+
+class MongoDB:
+    def __init__(self):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URI"))
+        self.db = self.client[os.getenv("DB_NAME", "nexa_otp")]
+        
+        # Auto-expire collections
+        self.db.otp_codes.create_index("expires_at", expireAfterSeconds=0)
+
+mongo = MongoDB()
